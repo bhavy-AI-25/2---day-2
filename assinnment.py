fd47f6f615410b859eb38143ab50c79b4ebf90e4
bhavy-AI-25 <preetamcharan@gmail.com>
2026-05-06 08:43:30 -0400

    my project assinment

---
+++ assinnment.py
@@ -0,0 +1,10 @@
+bir_time = "25/8/2015"
+Bir_day = "25 august"
+bIr_date = "25"
+biR_month = "august"
+bir_year = "2015"
+print(bir_time)
+print(Bir_day)
+print(bIr_date)
+print(biR_month)
+print(bir_year)
